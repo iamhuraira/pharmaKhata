08118577d4404b86b53f5ef9f762b9f889e4ef46
Abu Huraira <iamhuraira429@gmail.com>
2025-08-16 13:33:39 +0500

    chore: fix simple error

--- src/pages/api/products/index.ts
+++ src/pages/api/products/index.ts
@@ -1,6 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { connectDB } from '@/lib/db';
-import { Product, Category } from '@/lib/models';
+import { Product } from '@/lib/models';
 
 export default async function handler(
   req: NextApiRequest,
